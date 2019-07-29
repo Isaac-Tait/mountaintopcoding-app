9c95b680729ea377f553c39bd1a3d892e8505206
Isaac Tait <isaactait@Isaacs-MacBook-Air.local>
2019-07-29 11:11:21 -0700

    adjusting welcome message

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learning React one key stroke at a time
         </a>
       </header>
     </div>
