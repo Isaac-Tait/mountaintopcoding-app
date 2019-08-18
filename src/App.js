69f371f2938c5d1dcaddcbd58b1afcee8f6865dc
Isaac Tait <isaactait@Isaacs-MacBook-Air.local>
2019-08-17 21:28:23 -0700

    Getting in deep

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+function App() {
+
+};
+
 ReactDOM.render(
 <div>
   <h1>mountainTopCoding(&#9968;);</h1>
@@ -8,29 +12,4 @@ ReactDOM.render(
 
 document.getElementById('root'));
 
-/* import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-        mountainTopCoding(&#9968;);
-        </p>
-        <a
-          className="App-link"
-          href="https://www.mountaintopcoding.com/film-photography"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learning React one key stroke at a time
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App; 
-*/
+export default App;
