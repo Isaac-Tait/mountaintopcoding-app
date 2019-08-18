878bbe288fcd97fc5be73f06da5d09ca31f18f06
Isaac Tait <isaactait@Isaacs-MacBook-Air.local>
2019-08-17 20:47:09 -0700

    scrimba.com course

--- src/App.js
+++ src/App.js
@@ -8,11 +8,11 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+        mountainTopCoding(&#9968;);
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://www.mountaintopcoding.com/film-photography"
           target="_blank"
           rel="noopener noreferrer"
         >
