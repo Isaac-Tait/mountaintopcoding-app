e275a1014d9a381ed3b25f107f6624e93a8f1a74
Isaac Tait <isaactait@Isaacs-MacBook-Air.local>
2019-08-18 21:58:18 -0700

    Diving in head first... Is it shallow?

--- src/App.js
+++ src/App.js
@@ -1,14 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import LetsGo from 'lets-go';
 
-function App() {
- return ( 
- <div>
-    <h1>mountainTopCoding(&#9968;);</h1>
-</div>
-  )
-};
-
-ReactDOM.render(<App />, document.getElementById('root'));
-
-export default App;
+ReactDOM.render(<LetsGo />, document.getElementById('root'));
---
+++ src/LetsGo.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function LetsGo() {
+    return (
+    <div>
+        <h1>mountainTopCoding(&#9968;);</h1>
+    </div>
+    )
+};
+
+export default LetsGo;
