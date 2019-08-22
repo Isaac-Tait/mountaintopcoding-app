6217c8afac6ac53100061bad98b9b831cac22315
Isaac Tait <isaactait@Isaacs-MacBook-Air.local>
2019-08-21 22:10:49 -0700

    Got it working after some mass deletions

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import LetsGo from 'lets-go';
+import LetsGo from './LetsGo.js';
 
 ReactDOM.render(<LetsGo />, document.getElementById('root'));
---
+++ src/index.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import LetsGo from './LetsGo.js';
+
+ReactDOM.render(<LetsGo />, document.getElementById('root'));
