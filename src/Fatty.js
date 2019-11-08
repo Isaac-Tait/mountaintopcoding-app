6a5cece9fb1e5a71173187caac8291ec51617b48
Isaac Tait <isaac@mountaintopcoding.com>
2019-11-07 17:21:44 -0800

    building my form step 1

--- src/Fatty.js
+++ src/Fatty.js
@@ -6,9 +6,30 @@ class Fatty extends Component {
         location: ""
     }    
 
+handleChange(event) {
+    const {name} = event.target
+    type === "checkbox" ? this.setState({ [name]: checked }) : this.setState({ [name]: value })
+}
+
 render() {
     return (
         <main>
+            <input
+                type="text"
+                value={this.state.firstName}
+                name="firstName"
+                placeholder="Your first name"
+                onChange={this.handleChange}
+            />
+
+            <input 
+                type="text"
+                value={this.state.location}
+                name="location"
+                placeholder="Where are you from?"
+                onChange={this.handleChange}
+            />
+            
             <h1>mountainTopCoding(&#9968;);</h1>
             <p>Welcome {this.state.firstName} from {this.state.location} to mountainTopCoding(&#9968;);</p>
         </main>
