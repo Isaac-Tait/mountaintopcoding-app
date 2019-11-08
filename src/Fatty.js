1230ccfa3021c085296487d1df00cd83299466ea
Isaac Tait <isaac@mountaintopcoding.com>
2019-11-08 15:33:23 -0800

    building spa functionality

--- src/Fatty.js
+++ src/Fatty.js
@@ -20,22 +20,8 @@ render() {
         <main>
             <Header />
             <NavBar />
-        
-            <input
-                type="text"
-                value={this.state.firstName}
-                name="firstName"
-                onChange={this.handleChange}
-            /> What is your first name?
-                <br />
-            <input 
-                type="text"
-                value={this.state.location}
-                name="location"
-                onChange={this.handleChange}
-            /> Where are you from?
-                <br />
-        <p>Welcome {this.state.firstName} from {this.state.location} to mountainTopCoding(&#9968;);</p>
+            <Home />
+            
         </main>
         )
     }
--- src/Home.js
+++ src/Home.js
@@ -1,1 +1,25 @@
+import React from 'react'
 
+function Home() {
+    return (
+        <div>
+            <input
+                type="text"
+                value={this.state.firstName}
+                name="firstName"
+                onChange={this.handleChange}
+            /> What is your first name?
+            <br />
+            <input 
+                type="text"
+                value={this.state.location}
+                name="location"
+                onChange={this.handleChange}
+            /> Where are you from?
+            <br />
+            <p>Welcome {this.state.firstName} from {this.state.location} to mountainTopCoding(&#9968;);</p>
+        </div>
+    )
+}
+
+export default Home
