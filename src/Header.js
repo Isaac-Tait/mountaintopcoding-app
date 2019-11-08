77025b4a9178cd4dd9c8b0a965c0e731916d8489
Isaac Tait <isaac@mountaintopcoding.com>
2019-11-08 15:12:15 -0800

    it is hard to come up with meaningful commit msgs!

--- src/Header.js
+++ src/Header.js
@@ -3,7 +3,6 @@ import React from 'react'
 function Header() {
     return (
         <div>
-            <title>mountainTopCoding(&#9968;);</title>
             <h1>mountainTopCoding(&#9968;);</h1>
         </div>
         
