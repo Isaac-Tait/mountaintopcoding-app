b08af4b7853caa39a57a68b5a5d4669d04db7f96
Isaac Tait <isaac@mountaintopcoding.com>
2019-11-08 21:25:49 -0800

    So close to getting state to work

--- src/Home.js
+++ src/Home.js
@@ -1,27 +1,34 @@
 import React, {Component} from 'react'
 
 class Home extends Component {
-    render() {
-        return (
-            <div>
-              <input
-                    type="text"
-                    value={this.state.firstName}
-                    name="firstName"
-                    onChange={this.handleChange}
-                /> What is your first name?
-                <br />
-                <input 
-                    type="text"
-                    value={this.state.location}
-                    name="location"
-                    onChange={this.handleChange}
-                /> Where are you from?
-                <br />
-                <p>Welcome {this.state.firstName} from {this.state.location} to mountainTopCoding(&#9968;);</p>  
-            </div>
-        )
+    constructor() {
+        super()
+            this.state ={
+                firstName: "",
+                location: ""
+            }
+        }
+        render() {
+            return (
+                <div>
+                  <input
+                        type="text"
+                        value={this.state.firstName}
+                        name="firstName"
+                        onChange={this.handleChange}
+                    /> What is your first name?
+                    <br />
+                    <input 
+                        type="text"
+                        value={this.state.location}
+                        name="location"
+                        onChange={this.handleChange}
+                    /> Where are you from?
+                    <br />
+                    <p>Welcome {this.state.firstName} from {this.state.location} to mountainTopCoding(&#9968;);</p>  
+                </div>
+            )
+        }
     }
-}
 
 export default Home
