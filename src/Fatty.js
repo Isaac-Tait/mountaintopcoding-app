74d612b692e826777a716b89e521f2b6dd3f0c12
Isaac Tait <isaac@mountaintopcoding.com>
2019-11-09 20:45:53 -0800

    Header should be a link/ working on spa function

--- src/Fatty.js
+++ src/Fatty.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import NavBar from './NavBar.js'
 import Header from './Header.js'
 
@@ -7,6 +7,13 @@ function Fatty() {
         <main>
             <Header />
             <NavBar />
+
+            <p>
+                Over the last three or four years I have built three websites <a href="http://www.fallfishtenkara.com" target="_blank" class="external-link" rel="noopener noreferrer">Fallfish Tenkara</a>, 
+                <a href="http://www.cutthroattenkara.com" target="_blank" class="external-link" rel="noopener noreferrer">Cutthroat Tenkara</a>, & <a href="http://www.macadamgrinding.com" target="_blank" class="external-link" rel="noopener noreferrer">Macadam Grinding</a> 
+                using the venerable content management system - Wordpress. Through that process of building those sites I have come to be curious how Wordpress, and all of its millions of plugins, actually work. 
+                As a stay at home dad I would like to be able to learn a skill that I can use to "make some bread" without having to leave home. So, using Digital Ocean and Codecademy I have jumped in head first, so to speak, into the coding world.
+            </p>
             
         </main>
         )
--- src/index.js
+++ src/index.js
@@ -2,4 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Fatty from './Fatty.js';
 
+// function index() {
+//     return (
+//         <div>
+//             <p>
+//                 Over the last three or four years I have built three websites <a href="http://www.fallfishtenkara.com" target="_blank" class="external-link" rel="noopener noreferrer">Fallfish Tenkara</a>, 
+//                 <a href="http://www.cutthroattenkara.com" target="_blank" class="external-link" rel="noopener noreferrer">Cutthroat Tenkara</a>, & <a href="http://www.macadamgrinding.com" target="_blank" class="external-link" rel="noopener noreferrer">Macadam Grinding</a> 
+//                 using the venerable content management system - Wordpress. Through that process of building those sites I have come to be curious how Wordpress, and all of its millions of plugins, actually work. 
+//                 As a stay at home dad I would like to be able to learn a skill that I can use to "make some bread" without having to leave home. So, using Digital Ocean and Codecademy I have jumped in head first, so to speak, into the coding world.
+//             </p>
+//         </div> 
+//     )
+// }
+
 ReactDOM.render(<Fatty />, document.getElementById('root'));
