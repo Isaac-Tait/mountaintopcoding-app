2326607e32fbf10ec1378e21118edf3175dfafd1
Isaac Tait <isaac@mountaintopcoding.com>
2019-11-09 21:22:55 -0800

    added an image to the header container

--- src/Header.js
+++ src/Header.js
@@ -4,6 +4,7 @@ function Header() {
     return (
         <div>
             <h1>mountainTopCoding(&#9968;);</h1>
+            <img src="https://mountaintop-coding.s3-us-west-1.amazonaws.com/images/Mountain+Top.JPG"alt="Mt. Baldy as seen from Pine Peak near Los Angeles"/>
         </div>
         
     )
