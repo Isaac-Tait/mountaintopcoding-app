4140533e94f5929704062742112c39109973e13f
Isaac Tait <isaac@mountaintopcoding.com>
2019-11-09 20:59:46 -0800

    more updates to the landing page

--- src/Fatty.js
+++ src/Fatty.js
@@ -9,21 +9,23 @@ function Fatty() {
             <NavBar />
 
             <p>
-                Over the last three or four years I have built three websites <a href="http://www.fallfishtenkara.com" target="_blank" class="external-link" rel="noopener noreferrer">Fallfish Tenkara</a>, 
-                <a href="http://www.cutthroattenkara.com" target="_blank" class="external-link" rel="noopener noreferrer">Cutthroat Tenkara</a>, & <a href="http://www.macadamgrinding.com" target="_blank" class="external-link" rel="noopener noreferrer">Macadam Grinding</a> 
+                Over the last three or four years I have built three websites <a href="http://www.fallfishtenkara.com" target="_blank" class="external-link" rel="noopener noreferrer">Fallfish Tenkara, </a> 
+                <a href="http://www.cutthroattenkara.com" target="_blank" class="external-link" rel="noopener noreferrer">Cutthroat Tenkara</a>, & <a href="http://www.macadamgrinding.com" target="_blank" class="external-link" rel="noopener noreferrer">Macadam Grinding </a> 
                 using the venerable content management system - Wordpress. Through that process of building those sites I have come to be curious how Wordpress, and all of its millions of plugins, actually work. 
-                As a stay at home dad I would like to be able to learn a skill that I can use to "make some bread" without having to leave home. So, using Digital Ocean, freeCodeCamp, Codecademy, Coursera, and Scrimba I have jumped in head first into learning to write a plethora of coding languages.
+                On top of that curiousity, as a stay at home dad I of a toddler I began to look for job opportunities that I could pursue while remaining at home with my son. So, using Digital Ocean, AWS, Netlify, freeCodeCamp, Codecademy, Coursera, and Scrimba 
+                I have jumped in head first into teaching myself to become a full stack web developer.
             </p>
-            <h4>Thank you for visiting and stay tuned as I bring my newfound skills into a tangible medium</h4><br></br>
-
+            <h4>Thank you for visiting and stay tuned as I bring my newfound skills into a tangible medium</h4>
             <h2>My Roadmap</h2>
 	        <ol class="coding">
-                <li><strong>Learn HTML5</strong> - I first learned HTML2 from a friends grandpa who taught a homeschoolers curriculum. His granddaughter, Takara, was my first real crush. My original motivation for attending his class was in the hopes of catching a glimpse of Takara. Problem was I was too scared to look at her...</li>
-                <li><strong>Learn CSS</strong> - At first I was very intimidated by CSS. Just looking at it I had <strong>NO</strong> idea what the heck was going on. However, in just a few short days of using Codecademy my understanding and comfort with CSS has grown exponentially.</li>
-                <li><strong>Launch this website</strong> - I know I need to do something about my ever growing collection of Digital Ocean Droplets and URL's. Perhaps I will create a web developers anonymous group...</li>
-                <li><strong>Learn JavaScript</strong> - I tried once before using a book I purchased at Barnes & Noble. The book was thicker than my Bible and <strong>much</strong> more dry. Let us hope this time around the learning process goes a little better...</li>
-                <li><strong>Learn React</strong>After much debate and innner turmoil I finally settled on a framework to focus the lionshare of my learning efforts on. </li>
-                <li><strong>Fine tune my web development skills with a lot of practice.</strong> This is the fun part. To keep track of my learning I created a Journal of my learning progress.</li>
+                <li><strong>Learn HTML5</strong> - I first learned HTML2 from a friends grandpa who taught a homeschoolers curriculum. His granddaughter, Takara, was my first real crush. My original motivation for attending his class was in the hopes of catching a glimpse of Takara. Problem was I was too scared to look at her.</li>
+                <li><strong>Learn CSS</strong> - At first I was very intimidated by CSS. Just looking at it I had <strong>NO</strong> idea what the heck was going on. However, in just a few short days of using Codecademy my understanding and comfort with CSS has grown exponentially. I have continued to build my knowledge and 
+                    understanding of CSS through the great course offerings from freeCodeCamp and Scrimba.</li>
+                <li><strong>Launch this website</strong> - I know I need to do something about my ever growing collection of Digital Ocean Droplets and URL's. Perhaps I will create a web developers anonymous group.</li>
+                <li><strong>Learn JavaScript</strong> - I tried once before using a book I purchased at Barnes & Noble. The book was thicker than my Bible and <strong>much</strong> more dry. Let us hope this time around the learning process goes a little better.</li>
+                <li><strong>Learn React</strong>After much debate and innner turmoil I finally settled on a framework to focus the lionshare of my learning efforts on.</li>
+                <li><strong>Continue to fine tune my web development skills with a lot of practice.</strong> This is the part where I get to put all my newfound skills to work in one place. It has been fun to keep track of my learning journey by journaling almost everyday. 
+                    some days, and even some weeks, I have struggled to even turn my computer on. While other weeks I stay up for hours at night furiously banging away on my keyboard.</li>
             </ol>
         </main>
         )
