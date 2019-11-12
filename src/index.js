8b9984319bf77679d9a8559d3aa92f577670a714
Isaac Tait <isaac@mountaintopcoding.com>
2019-11-11 19:49:18 -0800

    testing... if the site breaks revert to here

--- src/index.js
+++ src/index.js
@@ -2,6 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 import Main from './Main.js'
-import Style from './style.css'
+
 
 ReactDOM.render(<Main />, document.getElementById("root"))
