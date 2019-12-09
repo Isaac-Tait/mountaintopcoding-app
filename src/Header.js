647d970067355454913c16619526273e70a72845
Isaac Tait <isaac@mountaintopcoding.com>
2019-12-08 21:24:29 -0800

    IndieWeb login will you work now?

--- src/Header.js
+++ src/Header.js
@@ -4,16 +4,7 @@ function Header() {
     return (
         <div className="logo">
             <h1>mountainTopCoding(&#9968;);</h1>
-            <ul>
-                <li>
-                    <a href="https://twitter.com/Isaac_Tait_83" rel="me">Twitter</a>
-                </li>
-                <li>
-                    <a href="https://github.com/Isaac-Tait" rel="me">Github</a>
-                </li>
-            </ul>
         </div>
-        
     )
 }
 
