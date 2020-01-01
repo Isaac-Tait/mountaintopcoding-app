f03af59e347e9bd6cb0a98cc26309a51cf26aca4
Isaac Tait <isaac@mountaintopcoding.com>
2019-12-31 17:40:04 -0800

    syntax updates

--- src/Coding.js
+++ src/Coding.js
@@ -8,9 +8,9 @@ class Coding extends Component {
                     class="codepen" data-height="265" data-theme-id="default" data-default-tab="css,result" data-user="isaac-tait" data-slug-hash="BaywrNQ" 
                     style="height: 265px; box-sizing: border-box; display: flex; align-items: center; justify-content: center; border: 2px solid; margin: 1em 0; padding: 1em;" 
                     data-pen-title="Ying Yang">
-                    <span>See the Pen <a href="https://codepen.io/isaac-tait/pen/BaywrNQ">
-                    Ying Yang</a> by Isaac Tait (<a href="https://codepen.io/isaac-tait">@isaac-tait</a>)
-                    on <a href="https://codepen.io">CodePen</a>.</span>
+                    <span>
+                        See the Pen <a href="https://codepen.io/isaac-tait/pen/BaywrNQ">Ying Yang</a> by Isaac Tait (<a href="https://codepen.io/isaac-tait">@isaac-tait</a>) on <a href="https://codepen.io">CodePen</a>.
+                    </span>
                 </p>
                     <script async src="https://static.codepen.io/assets/embed/ei.js"></script>
             </div>
