e8156b31f04966735d3d84f33ea8353234e23589
Isaac Tait <isaac@mountaintopcoding.com>
2020-01-06 20:28:57 -0800

    experiementing w/ jsx code changes

--- src/Coding.js
+++ src/Coding.js
@@ -5,10 +5,11 @@ class Coding extends Component {
         return (
             <div>
                 <p className="codepen" data-height={265} data-theme-id="default" data-default-tab="css,result" data-user="isaac-tait" data-slug-hash="BaywrNQ" style={{height: 265, boxSizing: 'border-box', display: 'flex', alignItems: 'center', justifyContent: 'center', border: '2px solid', margin: '1em 0', padding: '1em'}} data-pen-title="Ying Yang">
-  <span>
-    See the Pen <a href="https://codepen.io/isaac-tait/pen/BaywrNQ">Ying Yang</a> by Isaac Tait (<a href="https://codepen.io/isaac-tait">@isaac-tait</a>) on <a href="https://codepen.io">CodePen</a>.
-  </span>
+  <span>See the Pen <a href="https://codepen.io/isaac-tait/pen/BaywrNQ">
+      Ying Yang</a> by Isaac Tait (<a href="https://codepen.io/isaac-tait">@isaac-tait</a>)
+    on <a href="https://codepen.io">CodePen</a>.</span>
 </p>
+
             </div>
         )
     }
