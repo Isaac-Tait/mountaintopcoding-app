0f5e1298916bd64be6be99fdba04c08c58ed3c23
Isaac Tait <isaac@mountaintopcoding.com>
2020-01-06 20:59:52 -0800

    finally got codepen embed to render in the browser

--- src/Coding.js
+++ src/Coding.js
@@ -4,12 +4,14 @@ class Coding extends Component {
     render() {
         return (
             <div>
-                <p className="codepen" data-height={265} data-theme-id="default" data-default-tab="css,result" data-user="isaac-tait" data-slug-hash="BaywrNQ" style={{height: 265, boxSizing: 'border-box', display: 'flex', alignItems: 'center', justifyContent: 'center', border: '2px solid', margin: '1em 0', padding: '1em'}} data-pen-title="Ying Yang">
-  <span>See the Pen <a href="https://codepen.io/isaac-tait/pen/BaywrNQ">
-      Ying Yang</a> by Isaac Tait (<a href="https://codepen.io/isaac-tait">@isaac-tait</a>)
-    on <a href="https://codepen.io">CodePen</a>.</span>
-</p>
+                <h2>#Day1 Challenge</h2>
+                <iframe height={265} style={{width: '100%'}} scrolling="no" title="Ying Yang" 
+                src="https://codepen.io/isaac-tait/embed/preview/BaywrNQ?height=265&theme-id=default&default-tab=css,result" 
+                frameBorder="no" allowTransparency="true" allowFullScreen="true">
 
+                    <span>See the Pen <a href='https://codepen.io/isaac-tait/pen/BaywrNQ'>Ying Yang</a>by Isaac Tait
+                    (<a href='https://codepen.io/isaac-tait'>@isaac-tait</a>) on <a href='https://codepen.io'>CodePen</a></span>.
+                </iframe>
             </div>
         )
     }
