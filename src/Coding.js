cb1544e78b1d78cc197a4933a771f81693f5cdda
Isaac Tait <isaac@mountaintopcoding.com>
2020-01-28 14:16:10 -0800

    added #dayTwoChallenge

--- src/Coding.js
+++ src/Coding.js
@@ -4,13 +4,20 @@ class Coding extends Component {
     render() {
         return (
             <div>
-                <h2>#Day1 Challenge</h2>
+                <h2>#dayOneChallenge</h2>
                 <iframe height={265} style={{width: '100%'}} scrolling="no" title="Ying Yang" 
                 src="https://codepen.io/isaac-tait/embed/preview/BaywrNQ?height=265&theme-id=default&default-tab=css,result" 
                 frameBorder="no" allowTransparency="true" allowFullScreen="true">
 
                     <span>See the Pen <a href='https://codepen.io/isaac-tait/pen/BaywrNQ'>Ying Yang</a>by Isaac Tait
                     (<a href='https://codepen.io/isaac-tait'>@isaac-tait</a>) on <a href='https://codepen.io'>CodePen</a></span>.
+                </iframe>
+
+                <h2>#dayTwoChallenge</h2>
+                <iframe height="265" style="width: 100%;" scrolling="no" title="Kilometer/Mile Splits Calculator" 
+                src="https://codepen.io/isaac-tait/embed/rNabNgY?height=265&theme-id=default&default-tab=html,result" frameborder="no" allowtransparency="true" allowfullscreen="true">
+                See the Pen <a href='https://codepen.io/isaac-tait/pen/rNabNgY'>Kilometer/Mile Splits Calculator</a> by Isaac Tait
+                (<a href='https://codepen.io/isaac-tait'>@isaac-tait</a>) on <a href='https://codepen.io'>CodePen</a>.
                 </iframe>
             </div>
         )
