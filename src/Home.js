b320eaff02c20fb4b5cf2ed497c4a142ddd87618
Isaac Tait <isaac@mountaintopcoding.com>
2020-08-26 17:01:19 -0700

    bizzare tailwind happenings... 👻

--- src/Home.js
+++ src/Home.js
@@ -39,7 +39,7 @@ class Home extends Component {
                     /> 
                     <br />
                     <p class="mt-6 p-2">Welcome <span class="text-red-500">{this.state.firstName ? this.state.firstName : "_______"}</span> from <span class="text-red-500">{this.state.location ? this.state.location : "________"}</span> to my portfolio site - mountainTopCoding(<span role="img" aria-label="mountain with snow-cap">&#127956;</span>);</p>
-                    <p class="mt-10 p-2">Lorem ipsum dolor sit amet, an mel eligendi argumentum, ea eum veri oblique omittam. Cu utinam facilisis sed, usu ex voluptatum temporibus, sed prima iriure consectetuer ex. 
+                    <p class="mt-10 p-2 bg-pink-200">Lorem ipsum dolor sit amet, an mel eligendi argumentum, ea eum veri oblique omittam. Cu utinam facilisis sed, usu ex voluptatum temporibus, sed prima iriure consectetuer ex. 
                         Ornatus disputando est at. Mucius noster expetendis te vim, vocent splendide at nam, in officiis similique his. Vim liberavisse concludaturque te, an omnis appareat vim, 
                         usu ex possim sententiae. Qui et inani accusata moderatius. Ut ignota disputationi usu, eam te explicari percipitur.
                         Ut est meliore vocibus. Accusamus patrioque mei cu. Vel an antiopam gubergren. Dolor dolorum ceteros pri ne, eos id adhuc iisque ancillae, quo verterem rationibus ex. 
