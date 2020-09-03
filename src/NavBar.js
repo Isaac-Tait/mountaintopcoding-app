f76c5aa7e4e4ef020862233fc0959141544a2c74
Isaac Tait <isaac@mountaintopcoding.com>
2020-09-02 21:39:45 -0700

    playing around with tabs

--- src/NavBar.js
+++ src/NavBar.js
@@ -16,29 +16,29 @@ import Coding from './Coding.js'
 function NavBar() {
     return (
       <HashRouter class="">
-        <div>
-          <p class="bg-orange-400 text-3xl pl-8">Topics</p>
-            <ul class="pl-12">
-              <li class="text-black hover:text-blue-800 w-32">
-                <NavLink to="/"><span role="img" aria-label="Home with garden">&#127969;</span> - Home</NavLink>
+        <div class="">
+          <p class="text-3xl pl-8 sm:w-full bg-orange-400 lg:bg-pink-200">Topics</p>
+            <ul class="lg:flex">
+              <li class="lg:-mb-px mr-1">
+                <NavLink class="bg-pink-200 inline-block lg:bg-orange-400 lg:border-l lg:border-t lg:border-r lg:rounded-t lg:py-2 lg:px-4 font-semibold text-black hover:text-blue-800 w-32" to="/"><span role="img" aria-label="Home with garden">&#127969;</span> - Home</NavLink>
               </li>
-              <li class="text-black hover:text-blue-800 w-32">
-                <NavLink to="/About"><span role="img" aria-label="Question mark">&#10067;</span> - About</NavLink>
+              <li class="lg:mr-1">
+                <NavLink class="bg-pink-200 inline-block lg:bg-orange-400 lg:border-l lg:border-t lg:border-r lg:rounded-t lg:py-2 lg:px-4 font-semibold text-black hover:text-blue-800 w-32"to="/About"><span role="img" aria-label="Question mark">&#10067;</span> - About</NavLink>
               </li>
-              <li class="text-black hover:text-blue-800 w-32">
-                <NavLink to="/Theology"><span role="img" aria-label="Scroll">&#128220;</span> - Theology</NavLink>
+              <li class="lg:mr-1">
+                <NavLink class="bg-pink-200 inline-block lg:bg-orange-400 lg:border-l lg:border-t lg:border-r lg:rounded-t lg:py-2 lg:px-4 font-semibold text-black hover:text-blue-800 w-40" to="/Theology"><span role="img" aria-label="Scroll">&#128220;</span> - Theology</NavLink>
               </li>
-              <li class="text-black hover:text-blue-800 w-40">
-                <NavLink to="/GravelCycling"><span role="img" aria-label="Man bicycling">&#128692;</span> - Gravel Cycling</NavLink>
+              <li class="lg:mr-1">
+                <NavLink class="bg-pink-200 inline-block lg:bg-orange-400 lg:border-l lg:border-t lg:border-r lg:rounded-t lg:py-2 lg:px-4 font-semibold text-black hover:text-blue-800 w-60" to="/GravelCycling"><span role="img" aria-label="Man bicycling">&#128692;</span> - Gravel Cycling</NavLink>
               </li>
-              <li class="text-black hover:text-blue-800 w-48">
-                <NavLink to="/BackcountrySkiing"><span role="img" aria-label="Skier">&#9975;</span> - Backcountry Skiing</NavLink>
+              <li class="lg:mr-1">
+                <NavLink class="bg-pink-200 inline-block lg:bg-orange-400 lg:border-l lg:border-t lg:border-r lg:rounded-t lg:py-2 lg:px-4 font-semibold text-black hover:text-blue-800 w-60" to="/BackcountrySkiing"><span role="img" aria-label="Skier">&#9975;</span> - Backcountry Skiing</NavLink>
               </li>
-              <li class="text-black hover:text-blue-800 w-24">
-                <NavLink to="/Tenkara"><span role="img" aria-label="Fishing pole with a fish">&#127907;</span> - Tenkara</NavLink>
+              <li class="lg:mr-1">
+                <NavLink class="bg-pink-200 inline-block lg:bg-orange-400 lg:border-l lg:border-t lg:border-r lg:rounded-t lg:py-2 lg:px-4 font-semibold text-black hover:text-blue-800 w-40" to="/Tenkara"><span role="img" aria-label="Fishing pole with a fish">&#127907;</span> - Tenkara</NavLink>
               </li>
-              <li class="text-black hover:text-blue-800 w-324">
-                <NavLink to="/Coding"><span role="img" aria-label="DNA">&#129516;</span> - Coding</NavLink>
+              <li class="lg:mr-1">
+                <NavLink class="bg-pink-200 inline-block lg:bg-orange-400 lg:border-l lg:border-t lg:border-r lg:rounded-t lg:py-2 lg:px-4 font-semibold text-black hover:text-blue-800 w-40" to="/Coding"><span role="img" aria-label="DNA">&#129516;</span> - Coding</NavLink>
               </li>
             </ul>
             <div>
