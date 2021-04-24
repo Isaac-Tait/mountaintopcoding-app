f2110904f968f0d5d2493fc4bd6568fdd7357070
Isaac Tait <isaac@mountaintopcoding.com>
2021-04-23 21:18:01 -0700

    add link to new .dev site

--- src/Image.js
+++ src/Image.js
@@ -9,6 +9,10 @@ function Image() {
                 src={Mountain}
                 alt="Mount Baldy's north flank viewed from Pine Peak near Los Angeles, California."
             />
+            <p class="font-semibold flex justify-center text-lg">
+                <span role="img" aria-label="waving hand">&#128075;&nbsp;&#128075;&nbsp;</span>
+                This is my old site, left here for posterity sake - go check out&nbsp;<a href="https://mountaintopcoding.dev" rel="noopener noreferrer" class="underline text-red-500">mountaintopcoding.dev</a>&nbsp;to see the new site.
+            </p>
         </div>
     )
 }
